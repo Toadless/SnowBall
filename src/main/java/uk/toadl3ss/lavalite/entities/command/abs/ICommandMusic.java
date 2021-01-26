a84ef3f9067b83bebc3a88ba5c40448bdb17451e
Toadless <thomasmcdonaldoctober@gmail.com>
2021-01-26 17:21:50 +0000

    d  o  c

--- src/main/java/uk/toadl3ss/lavalite/entities/command/abs/ICommandMusic.java
+++ src/main/java/uk/toadl3ss/lavalite/entities/command/abs/ICommandMusic.java
@@ -1,5 +1,8 @@
 package uk.toadl3ss.lavalite.entities.command.abs;
 
+/**
+ *  This interface represents each command from {@link uk.toadl3ss.lavalite.command.music music}.
+ */
 public interface ICommandMusic
 {
 }
