680059c76eb794798fedba6134d78ea4a05bc9e3
Toadless <thomasmcdonaldoctober@gmail.com>
2021-02-05 21:06:11 +0000

    D O C fixes

--- src/main/java/net/toaddev/lavalite/entities/command/CommandFlag.java
+++ src/main/java/net/toaddev/lavalite/entities/command/CommandFlag.java
@@ -25,33 +25,33 @@
 package net.toaddev.lavalite.entities.command;
 
 /**
- * An enum for flags related to {@link CommandManager command} execution.
+ * An enum for flags related to {@link net.toaddev.lavalite.modules.CommandsModule command} execution.
  */
 public enum CommandFlag
 {
     /**
-     * If the {@link CommandManager command} can only be used by developers.
+     * If the {@link net.toaddev.lavalite.modules.CommandsModule command} can only be used by developers.
      *
      * @see CommandEvent#isDeveloper().
      */
     DEVELOPER_ONLY,
     /**
-     * If the {@link CommandManager command} can only be used by server admins.
+     * If the {@link net.toaddev.lavalite.modules.CommandsModule command} can only be used by server admins.
      *
      * @see CommandEvent#isServerAdmin().
      */
     SERVER_ADMIN_ONLY,
     /**
-     * If the {@link CommandManager command} is disabled.
+     * If the {@link net.toaddev.lavalite.modules.CommandsModule command} is disabled.
      *
      */
     /**
-     * If the {@link CommandManager commands} message needs to be deleted.
+     * If the {@link net.toaddev.lavalite.modules.CommandsModule commands} message needs to be deleted.
      */
     AUTO_DELETE_MESSAGE,
 
     /**
-     * If the {@link CommandManager command} is disabled.
+     * If the {@link net.toaddev.lavalite.modules.CommandsModule command} is disabled.
      */
     DISABLED;
 }
